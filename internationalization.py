545f475458e3efe4b5316978fbe10fa4703f4401
Mehmet Aktaş <87446203+Drmehmetaktass@users.noreply.github.com>
2022-07-13 00:08:05 +0300

    Update internationalization.py

--- internationalization.py
+++ internationalization.py
@@ -41,7 +41,7 @@ class _Underscore(object):
             )
             for locale
             in available_locales.keys()
-            if locale != 'en_US'  # No translation file for en_US
+            if locale != 'tr_TR'  # No translation file for en_US
         }
         self.locale_stack = list()
 
